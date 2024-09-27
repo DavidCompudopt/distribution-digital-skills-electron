6d5f64515b2211e75f2e7859912f1bd9418ed356
DavidCompudopt <david.adeyemi@compudopt.org>
2024-09-27 10:19:22 -0500

    initial push

---
+++ src/courses.js
@@ -0,0 +1,348 @@
+const courses = [
+  {
+    id: 1,
+    language: 'en',
+    title: "Computer Basics",
+    description: "Phasellus ac tellus tincidunt, pharetra dui eu, bibendum nulla.",
+    lessons: [
+      {
+        id: 1,
+        title: "About your computer",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 2,
+        title: "Computer parts and functions",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 3,
+        title: "Getting started with your laptop",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "Using your Keyboard and touchpad",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "Intro to the taskbar",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "Using a file explorer",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "Using a usb drive",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "Start, settings, and updates",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+    ]
+  },
+  {
+    id: 2,
+    language: 'es',
+    title: "Conocimientos Básicos de Computadora",
+    description: "Cras ut sem eu ligula luctus ornare quis nec arcu.",
+    lessons: [
+      {
+        id: 1,
+        title: "Welcome to the course",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 2,
+        title: "How does a camera work?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 3,
+        title: "What is exposure?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "What is an aperture?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      }
+    ]
+  },
+  {
+    id: 3,
+    language: 'en',
+    title: "Email",
+    description: "Cras ut sem eu ligula luctus ornare quis nec arcu.",
+    lessons: [
+      {
+        id: 1,
+        title: "Welcome to the course",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 2,
+        title: "How does a camera work?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 3,
+        title: "What is exposure?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "What is an aperture?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      }
+    ]
+  },
+  {
+    id: 4,
+    language: 'es',
+    title: "Correo Electrónico",
+    description: "Cras ut sem eu ligula luctus ornare quis nec arcu.",
+    lessons: [
+      {
+        id: 1,
+        title: "Welcome to the course",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 2,
+        title: "How does a camera work?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 3,
+        title: "What is exposure?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "What is an aperture?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      }
+    ]
+  },
+  {
+    id: 5,
+    language: 'en',
+    title: "Internet Use and Safety",
+    description: "Cras ut sem eu ligula luctus ornare quis nec arcu.",
+    lessons: [
+      {
+        id: 1,
+        title: "Welcome to the course",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 2,
+        title: "How does a camera work?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 3,
+        title: "What is exposure?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "What is an aperture?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      }
+    ]
+  },
+  {
+    id: 6,
+    language: 'es',
+    title: "Uso y Seguridad de Internet",
+    description: "Cras ut sem eu ligula luctus ornare quis nec arcu.",
+    lessons: [
+      {
+        id: 1,
+        title: "Welcome to the course",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 2,
+        title: "How does a camera work?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 3,
+        title: "What is exposure?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "What is an aperture?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      }
+    ]
+  },
+  {
+    id: 7,
+    language: 'en',
+    title: "Tech for Business",
+    description: "Cras ut sem eu ligula luctus ornare quis nec arcu.",
+    lessons: [
+      {
+        id: 1,
+        title: "Welcome to the course",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 2,
+        title: "How does a camera work?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 3,
+        title: "What is exposure?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "What is an aperture?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      }
+    ]
+  },
+  {
+    id: 8,
+    language: 'es',
+    title: "Yecnología para los Negocios",
+    description: "Cras ut sem eu ligula luctus ornare quis nec arcu.",
+    lessons: [
+      {
+        id: 1,
+        title: "Welcome to the course",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 2,
+        title: "How does a camera work?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 3,
+        title: "What is exposure?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "What is an aperture?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      }
+    ]
+  },
+  {
+    id: 9,
+    language: 'en',
+    title: "Technology for Life",
+    description: "Cras ut sem eu ligula luctus ornare quis nec arcu.",
+    lessons: [
+      {
+        id: 1,
+        title: "Welcome to the course",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 2,
+        title: "How does a camera work?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 3,
+        title: "What is exposure?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "What is an aperture?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      }
+    ]
+  },
+  {
+    id: 10,
+    language: 'es',
+    title: "Tecnologia para la vida",
+    description: "Cras ut sem eu ligula luctus ornare quis nec arcu.",
+    lessons: [
+      {
+        id: 1,
+        title: "Welcome to the course",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 2,
+        title: "How does a camera work?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 3,
+        title: "What is exposure?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      },
+      {
+        id: 4,
+        title: "What is an aperture?",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam suscipit bibendum ex nec interdum.",
+        vimeoId: 76979871
+      }
+    ]
+  }
+]
+
+export default courses
